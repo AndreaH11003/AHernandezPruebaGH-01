af6bc022424a11313d9da179f9639469b5ffd742
AndreaH11003 <andrea.hernandez1@unisimon.edu.co>
2025-05-22 13:04:58 -0500

    proyecto con no peek

---
+++ A_Hernandez_Final/Assets/Samples/scripts/Mensajes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mensajes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Mensaje()
+    {
+        print("hola, este es el mensaje 1");
+    }
+    public void Mensaje2()
+    {
+        print("hola, este es el mensaje 2");
+    }
+    public void Mensaje3(string nombre)
+    {
+        print("hola"+nombre+"¿como estas?");
+    }
+
+}
